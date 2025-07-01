88ed4e385e8c458d8bba150f764b0a48d9b43988
kun1225 <kun881225@gmail.com>
2025-07-01 15:34:30 +0800

    update style

--- components/theme-title.vue
+++ components/theme-title.vue
@@ -4,7 +4,7 @@
   >
     <p
       v-if="chapter"
-      class="!leading-none absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10 text-[420px] font-bold font-antonio text-primary-highlight -skew-x-16 whitespace-nowrap opacity-25"
+      class="!leading-none absolute top-[44%] left-1/2 -translate-x-1/2 -translate-y-1/2 -z-10 text-[500px] font-bold font-antonio text-primary-highlight -skew-x-16 whitespace-nowrap opacity-25"
     >
       {{ `CH.${chapter}` }}
     </p>
